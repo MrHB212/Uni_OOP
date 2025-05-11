8bdc640f3bb17d5c29cc958a6ffbb3099f65a03c
MrHB212 <h.barut97@gmail.com>
2025-05-11 20:38:57 +0200

    ganze geraden zahlen

--- Ü01/GanzeZahlen.java
+++ Ü01/GanzeZahlen.java
@@ -2,10 +2,10 @@ public class GanzeZahlen {
     public static void main(String[] args) {
         int summe = 0; //Variable für die Summe
 
-        for (int a = 0; a != 10; a++){
+        for (int a = 0; a <= 20; a += 2){
             summe += a; //summe = summe + a
         }
 
-        System.out.println("Die Summe der ersten 10 ganzen Zahlen ab 0 : " + summe); //Ausgabe summe
+        System.out.println("Die Summe der ersten 10 ganzen geraden Zahlen : " + summe); //Ausgabe summe
     }
 }
