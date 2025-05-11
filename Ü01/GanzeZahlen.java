d19a6aca04d0df9769eb1dcefef962ddca143f1a
MrHB212 <h.barut97@gmail.com>
2025-05-11 20:45:10 +0200

    Update GanzeZahlen.java

--- Ü01/GanzeZahlen.java
+++ Ü01/GanzeZahlen.java
@@ -2,7 +2,7 @@ public class GanzeZahlen {
     public static void main(String[] args) {
         int summe = 0; //Variable für die Summe
 
-        for (int a = 0; a <= 20; a += 2){
+        for (int a = 0; a != 20; a += 2){
             summe += a; //summe = summe + a
         }
 
